5d627b9ccc671a1b96f4d3ae03ecfb5fdbcec4d7
Gagandeep Pratihar <gagandeep.pratihar@prismhr.com>
2025-04-21 14:48:39 +0000

    Code cleaned up

--- main.py
+++ main.py
@@ -3,32 +3,84 @@ import json
 from mqtt_helper import MqttHelper
 
 # Keep track of the list of devices that have already been published to or received from.
-devices_published_to, devices_received_from, devices_info = [],[],[]
+devices_published_to = []
+devices_received_from = []
+devices_info = []
+
 
 def on_connect(mqtt):
-    mqtt.subscribe('6718874bc1628f9b0dcd1ee7/+/connected', 2)
-    mqtt.subscribe('6718874bc1628f9b0dcd1ee7/+/aboutdevice/response', 2)
+    """
+    Callback function for MQTT connection.
+    Subscribes to necessary topics.
+    """
+    mqtt.subscribe('6718874bc1628f9b0dcd1ee7/+/connected', qos=2)
+    mqtt.subscribe('6718874bc1628f9b0dcd1ee7/+/aboutdevice/response', qos=2)
 
 
 def on_message(mqtt, topic, message):
-    # Split the topic into its components
+    """
+    Callback function for handling incoming MQTT messages.
+    Processes messages based on the topic structure.
+    """
     topic_split = topic.split('/')
-    company_code = topic_split[0]
-    serial_number = topic_split[1]
+    if len(topic_split) < 3:
+        return  # Invalid topic structure, ignore
 
-    if len(topic_split) == 3 and topic_split[2] == 'connected' and serial_number not in devices_published_to:
+    company_code, serial_number = topic_split[0], topic_split[1]
+
+    if len(topic_split) == 3 and topic_split[2] == 'connected':
+        handle_connected_topic(mqtt, company_code, serial_number)
+    elif len(topic_split) == 4 and topic_split[2] == 'aboutdevice' and topic_split[3] == 'response':
+        handle_aboutdevice_response(serial_number, message)
+
+
+def handle_connected_topic(mqtt, company_code, serial_number):
+    """
+    Handles the 'connected' topic.
+    Publishes a request to the 'aboutdevice' topic if the device is not already published to.
+    """
+    if serial_number not in devices_published_to:
         devices_published_to.append(serial_number)
-        mqtt.publish(f'{company_code}/{serial_number}/aboutdevice/request', '', 2)
-    elif len(topic_split) == 4 and topic_split[2] == 'aboutdevice' and topic_split[3] == 'response' and serial_number not in devices_received_from:
+        mqtt.publish(f'{company_code}/{serial_number}/aboutdevice/request', payload='', qos=2)
+
+
+def handle_aboutdevice_response(serial_number, message):
+    """
+    Handles the 'aboutdevice/response' topic.
+    Parses the message and updates the devices_info list.
+    """
+    if serial_number not in devices_received_from:
         devices_received_from.append(serial_number)
-        users = json.loads(message).get("users")
-         # Append the serial_number and users to the devices_info list
-        devices_info.append({"serial_number": serial_number, "users": users})
-        # print(f'{serial_number}: {users}')
+        try:
+            users = json.loads(message).get("users", [])
+            devices_info.append({"serial_number": serial_number, "users": users})
+        except json.JSONDecodeError:
+            print(f"Failed to decode JSON message: {message}")
+
+
+def parse_arguments():
+    """
+    Parses command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description="MQTT Device Manager")
+    parser.add_argument(
+        '-pf', '--product-flavor', required=True, help='The product flavor'
+    )
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main entry point of the script.
+    Initializes the MQTT helper with the appropriate callbacks.
+    """
+    args = parse_arguments()
+    MqttHelper(
+        on_connect_callback=on_connect,
+        product_flavor=args.product_flavor,
+        on_message_callback=on_message
+    )
+
 
 if __name__ == '__main__':
-    # Parse the command line arguments.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-pf', '--product-flavor', required=True, help='The product flavor')
-    args = parser.parse_args()
-    MqttHelper(on_connect_callback=on_connect, product_flavor=args.product_flavor, on_message_callback=on_message)
+    main()
--- mqtt_helper.py
+++ mqtt_helper.py
@@ -3,75 +3,104 @@
 import json
 import ssl
 import sys
-
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import error_string
 
-class MqttHelper:
 
-    def __init__(self, serial_number=None, company_code=None, product_flavor= None, on_company_code_callback=None, on_connect_callback=None, on_disconnect_callback=None, on_subscribe_callback=None, on_publish_callback=None, on_message_callback=None):
+class MqttHelper:
+    def __init__(
+        self,
+        serial_number=None,
+        company_code=None,
+        product_flavor=None,
+        on_company_code_callback=None,
+        on_connect_callback=None,
+        on_disconnect_callback=None,
+        on_subscribe_callback=None,
+        on_publish_callback=None,
+        on_message_callback=None,
+    ):
         self.serial_number = serial_number
         self.company_code = company_code
-        self.product_flavor = product_flavor.upper()
+        self.product_flavor = product_flavor.upper() if product_flavor else None
         self.on_company_code_callback = on_company_code_callback
         self.on_connect_callback = on_connect_callback
         self.on_disconnect_callback = on_disconnect_callback
         self.on_subscribe_callback = on_subscribe_callback
         self.on_publish_callback = on_publish_callback
         self.on_message_callback = on_message_callback
+
         self.mqtt_client = mqtt.Client()
+        self._initialize_mqtt_callbacks()
+
+        print("Connecting")
+        self._setup_tls_and_connect()
+        self._start_loop()
+
+    def _initialize_mqtt_callbacks(self):
+        """Initialize MQTT client callbacks."""
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_disconnect = self.on_disconnect
         self.mqtt_client.on_subscribe = self.on_subscribe
         self.mqtt_client.on_publish = self.on_publish
         self.mqtt_client.on_message = self.on_message
-        print('Connecting')
 
-        # Epay.
-        if self.product_flavor == 'EPAY':
-            self.mqtt_client.tls_set(ca_certs='certificate/epay_intermediate_certificate_authority.cer', certfile='certificate/client/epay_client_certificate.cer', keyfile='certificate/client/epay_client_certificate.key', cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS, ciphers=None)
-            self.mqtt_client.connect('mqttbroker.sekureid.com', 8883, 60)
+    def _setup_tls_and_connect(self):
+        """Set up TLS and connect to the MQTT broker."""
+        if self.product_flavor == "EPAY":
+            self.mqtt_client.tls_set(
+                ca_certs="certificate/epay_intermediate_certificate_authority.cer",
+                certfile="certificate/client/epay_client_certificate.cer",
+                keyfile="certificate/client/epay_client_certificate.key",
+                cert_reqs=ssl.CERT_REQUIRED,
+                tls_version=ssl.PROTOCOL_TLS,
+                ciphers=None,
+            )
+            self.mqtt_client.connect("mqttbroker.sekureid.com", 8883, 60)
 
-        # Loop indefinitely until the user presses the 'enter' button.
+    def _start_loop(self):
+        """Start the MQTT client loop."""
         try:
             self.mqtt_client.loop_start()
-            input()
+            input("Press Enter to exit...\n")
         finally:
-            self.mqtt_client.disconnect()
+            self.disconnect()
             sys.exit(0)
 
-    def on_connect(self, mqtt_client, userdata, flags, rc):
-        print('Connected')
+    def on_connect(self, mqtt_client):
+        """Handle MQTT connection."""
+        print("Connected")
         if self.on_connect_callback:
             self.on_connect_callback(self)
 
-        if self.serial_number != '' and self.on_company_code_callback:
-            mqtt_client.subscribe(f'+/{self.serial_number}/connected', 2)
-            print('Subscribed')
+        if self.serial_number and self.on_company_code_callback:
+            mqtt_client.subscribe(f"+/{self.serial_number}/connected", qos=2)
+            print("Subscribed to connected topic")
 
-    def on_disconnect(self, mqtt_client, userdata, rc):
+    def on_disconnect(self, rc):
+        """Handle MQTT disconnection."""
         if self.on_disconnect_callback:
             self.on_disconnect_callback()
         elif rc != mqtt.MQTT_ERR_SUCCESS:
-            print(f'Disconnected with result code: {error_string(rc)}')
+            print(f"Disconnected with result code: {error_string(rc)}")
 
-    def on_subscribe(self, mqtt_client, userdata, message_id, granted_qos, properties=None):
+    def on_subscribe(self,  message_id):
+        """Handle MQTT subscription acknowledgment."""
         if self.on_subscribe_callback:
             self.on_subscribe_callback(self, message_id)
 
-    def on_publish(self, mqtt_client, userdata, message_id):
+    def on_publish(self, message_id):
+        """Handle MQTT publish acknowledgment."""
         if self.on_publish_callback:
             self.on_publish_callback(self, message_id)
 
-    def on_message(self, mqtt_client, userdata, message):
-        payload = str(message.payload, 'utf-8')
-        # print(f'Received: {message.topic}: {payload}')
+    def on_message(self, message):
+        """Handle incoming MQTT messages."""
+        payload = message.payload.decode("utf-8")
+        topic_parts = message.topic.split("/")
 
-        split = message.topic.split('/')
-        if len(split) == 3 and split[1] == self.serial_number and split[2] == 'connected':
-            self.company_code = split[0]
-            # print(f'Company code: {company_code}')
-
+        if len(topic_parts) == 3 and topic_parts[1] == self.serial_number and topic_parts[2] == "connected":
+            self.company_code = topic_parts[0]
             if self.on_company_code_callback:
                 self.on_company_code_callback(self, self.company_code, self.serial_number)
             elif self.on_message_callback:
@@ -79,25 +108,33 @@ class MqttHelper:
         elif self.on_message_callback:
             self.on_message_callback(self, message.topic, payload)
         else:
-            try:
-                json_object = json.loads(payload)
-                print(f'Received: {message.topic}:\n{json.dumps(json_object, indent=2)}')
-            except ValueError:
-                print(f'Received: {message.topic}:\n{payload}')
-            mqtt_client.disconnect()
+            self._handle_unexpected_message(message, payload)
+
+    def _handle_unexpected_message(self, message, payload):
+        """Handle unexpected MQTT messages."""
+        try:
+            json_object = json.loads(payload)
+            print(f"Received: {message.topic}:\n{json.dumps(json_object, indent=2)}")
+        except ValueError:
+            print(f"Received: {message.topic}:\n{payload}")
+        self.mqtt_client.disconnect()
 
     def subscribe(self, topic, quality_of_service=2):
+        """Subscribe to an MQTT topic."""
         self.mqtt_client.subscribe(topic, quality_of_service)
-        print(f'Subscribed: {topic}')
+        print(f"Subscribed to: {topic}")
 
     def publish(self, topic, message, quality_of_service):
+        """Publish a message to an MQTT topic."""
         message_info = self.mqtt_client.publish(topic, message, quality_of_service)
         if not message_info.is_published:
             message_info.wait_for_publish()
-        print(f'Published: {topic}: {message}')
+        print(f"Published: {topic}: {message}")
 
     def disconnect(self):
+        """Disconnect the MQTT client."""
         self.mqtt_client.disconnect()
 
     def get_company_code(self):
+        """Get the company code."""
         return self.company_code
